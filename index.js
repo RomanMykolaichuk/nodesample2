e3be4f3c368c64b89caf5d5946053a5eb37a51ec
38068 <38068@DESKTOP-SRPFKQ2>
2020-11-01 18:43:38 +0200

    first commit

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+        bodyParser.urlencoded({
+            extended: true,
+        })
+        )
 
 app.get('/', (request, response) => {
-    
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+    response.set('Access-Control-Allow-Origin', '*');
+    response.json({info: 'Node.js, Express, and Postgres API'})
 })
 
 app.get('/users', db.getUsers)
@@ -22,5 +22,5 @@ app.get('/users/:id', db.getUserById)
 
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
+    console.log(`App running on port ${port}.`)
 })
